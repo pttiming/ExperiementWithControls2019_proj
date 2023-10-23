2ec5a2b7ec0cdb855b4c1edf9d78ad9fbc6b1ec2
pttiming <sean@pttiming.com>
2023-10-23 14:46:15 -0500

    Added additional textbox controls

--- ExperimentWithControls2019/MainWindow.xaml.cs
+++ ExperimentWithControls2019/MainWindow.xaml.cs
@@ -34,5 +34,31 @@ namespace ExperimentWithControls2019
         {
             e.Handled = !int.TryParse(e.Text, out int result);
         }
+
+        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString("0");
+        }
+
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = bigSlider.Value.ToString("000-000-0000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
     }
 }
